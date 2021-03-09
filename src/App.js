c32e64d09507a5138ab0a1214efb30dea0377d12
WebSpider Tutorials <41290114+abhi283204@users.noreply.github.com>
2021-03-09 10:52:16 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import React, { useState } from "react";
+
 import "./styles.css";
 
-export default function App() {
+function App() {
+  const [name, setname] = useState("Abhi");
   return (
     <div className="App">
-      <h1>Hello Abhi</h1>
+      <h1>{name}</h1>
       <h2>This is Final update</h2>
-
       <h3>Magic happened</h3>
-
       <p>Abhishek edited the fork</p>
-
       <p>Abhishek edited the fork another time...!!!!-----</p>
-
       <h3>Again Magic happened---</h3>
     </div>
   );
 }
+
+export default App;
